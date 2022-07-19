1bafa0fa2e67270cadf13b0fc343e633f95aacbb
suraj <sk5620980@gmail.com>
2022-07-19 17:32:17 +0530

    Updates

---
+++ precache-manifest.bb1c8c4aa09912b926246b3ce592b5ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c62d020a475237c9ceef",
+    "url": "/my_resume.github.io/static/css/main.c2269dd0.chunk.css"
+  },
+  {
+    "revision": "c62d020a475237c9ceef",
+    "url": "/my_resume.github.io/static/js/main.c62d020a.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/my_resume.github.io/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "296fb87d258921a555e8",
+    "url": "/my_resume.github.io/static/js/2.296fb87d.chunk.js"
+  },
+  {
+    "revision": "9600d9c081c3242b494e",
+    "url": "/my_resume.github.io/static/js/runtime~main.9600d9c0.js"
+  },
+  {
+    "revision": "2040e5febe88761d83e10ef1e7eee6b1",
+    "url": "/my_resume.github.io/index.html"
+  }
+];
